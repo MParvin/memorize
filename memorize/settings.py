28d4aa797677acc2897286c8ac278182b0cea8b7
Mohammad Parvin <m.parvin986@gmail.com>
2022-08-05 17:22:39 -0400

    add template dir and static dir

--- memorize/settings.py
+++ memorize/settings.py
@@ -51,11 +51,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'memorize.urls'
+TEMPLATES_DIRS = os.path.join(BASE_DIR, 'templates')
+
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIRS],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
